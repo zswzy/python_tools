60b1408a7ae90c6d3012890eba2a15cee901f009
zswzy <aircn33450@126.com>
2021-06-09 09:44:42 +0800

    Update sparse_csv.py

--- sparse_csv/sparse_csv.py
+++ sparse_csv/sparse_csv.py
@@ -3,12 +3,22 @@
 import os
 import csv
 
-filename_dense= r"C:\Users\Zeyuan\Desktop\sparse csv\trajactory_log.csv"
-filename_sparse = r"C:\Users\Zeyuan\Desktop\sparse csv\trajactory_log_sparse.csv"
+filepath_dense= r"C:\Users\Zeyuan\Desktop\sparse csv\trajactory_log.csv"
+filepath_dense = input("请拖入原始文件： ")
+if filepath_dense[0] == '"' or filepath_dense[0] == "'":
+    filepath_dense = filepath_dense[1: len(filepath_dense)-1]
+elif filepath_dense[0] == "&":
+    filepath_dense = filepath_dense[3: len(filepath_dense)-1]
 
-with open(filename_dense, newline='') as densefile:
+dir = os.path.dirname(filepath_dense)  # 获取文件夹名称
+filename_dense = os.path.basename(filepath_dense)  # 获取文件名称
+name_dense, ext= os.path.splitext(filename_dense)  # 文件名和扩展名
+filename_sparse = name_dense + "_sparse" + ext
+filepath_sparse = os.path.join(dir, filename_sparse)  # 在原始路径下生成sparse文件
+
+with open(filepath_dense, newline='') as densefile:
     reader = csv.reader(densefile, delimiter=',')
-    with open(filename_sparse, 'w', newline='') as sparsefile:
+    with open(filepath_sparse, 'w', newline='') as sparsefile:
         writer = csv.writer(sparsefile, delimiter=',')
         
         i = 0
